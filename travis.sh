1909f3cfb8f9839ea27fca6f766829b934dc3505
onoie <onoie3@gmail.com>
2017-11-01 18:09:20 +0900

    fast commit

--- travis.sh
+++ travis.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 getName(){ unzip -qql $1 | head -n1 | tr -s ' ' | cut -d' ' -f5- | rev | cut -c 2- | rev }
 wget --user=$USER --password=$PASS $URL
+ls
 DIR=$(getName master.zip)
-unzip master.zip
-mv $DIR src
-ls
-cd src
-ls
+echo $DIR
 
 echo "complete"
 
